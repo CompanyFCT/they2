4e680238be1304ccb83bb59c0d4a0ed5e1cd56f3
Fausto <fausto.alemao@gmail.com>
2013-06-19 18:50:45 -0300

    adding model backend validation length data

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+validate = require('mongoose-validator').validate;
+
 module.exports = mongoose.model('User', new mongoose.Schema({
-  name: {type: String, required: true},
-  email: {type: String, required: true},
-  phone: {type: String, required: true},
+  name: {type: String, required: true, validate: validate({message: "Exceeded maxlength"}, 'len', 5, 50)},
+  email: {type: String, required: true, validate: validate({message: "Exceeded maxlength"}, 'len', 5, 50)},
+  phone: {type: String, required: true, validate: validate({message: "Exceeded maxlength"}, 'len', 5, 15)},
   when: {type: Date, default: Date.now, required: true}
 }));
